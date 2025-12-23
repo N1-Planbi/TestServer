95658c5495c50a31c155767a9fefe2975ed71ef1
PLANBI <>
2025-12-23 21:49:18 +0900

    점프기능 추가 테스트 ver1.0.1

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,15 +1,54 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    public float groundCheckDistance = 0.3f;
+    public LayerMask groundMask;
+
+    private Rigidbody rb;
+    private bool jumpPressed;
+    private Vector3 inputDir;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
 
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal"); // A/D, Left/Right
-        float v = Input.GetAxisRaw("Vertical");   // W/S, Up/Down
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        inputDir = new Vector3(h, 0, v).normalized;
 
-        Vector3 dir = new Vector3(h, 0f, v).normalized;
-        transform.position += dir * moveSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("점프");
+            jumpPressed = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 이동
+        Vector3 velocity = inputDir * moveSpeed;
+        rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
+
+        // 점프
+        if (jumpPressed && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+
+        jumpPressed = false;
+    }
+
+    private bool IsGrounded()
+    {
+        return true; // 임시로 항상 땅 위라고 가정
     }
 }
